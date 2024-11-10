f0103ad32e386cbe74cee09d0c6561f082b35c23
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2024-11-10 20:44:24 +0100

    src/tt_um_qosc.v info.yaml test/Makefile : add registers and reassign I/O

--- src/tt_um_qosc.v
+++ src/tt_um_qosc.v
@@ -74,6 +74,6 @@ quadrature_oscillator_sync qosc(
   assign uio_oe  = 8'hff;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, ui_in[7:2], uio_in, 1'b0};
+  wire _unused = &{ena, ui_in[5:3], 1'b0};
 
 endmodule
